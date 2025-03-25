13d6794c329e3f128c2f9da5e6f8c7ae86f5a9fb
Matias <mmatidelga2@gmail.com>
2025-03-25 19:48:45 -0300

    feat: add draft register page component

---
+++ src/app/pages/auth/register-page/register-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'f5pi-register-page',
+  imports: [],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.css',
+})
+export class RegisterPageComponent {}
