c7dfd08f6947791330c6aae583df504f82e8aa03
Matias <mmatidelga2@gmail.com>
2025-03-30 21:21:27 -0300

    feat: implement sidenav component with tooltip support

--- src/app/common/components/sidenav-item/sidenav-item.component.ts
+++ src/app/common/components/sidenav-item/sidenav-item.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterModule, MatIconModule],
+  imports: [RouterModule, MatIconModule, MatButtonModule, MatTooltipModule],
   selector: 'f5pi-sidenav-item',
   styleUrl: './sidenav-item.component.css',
   templateUrl: './sidenav-item.component.html',
