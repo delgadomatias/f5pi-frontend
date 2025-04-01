d8e145f4417d62038405a8c3b8b6e8be51e655f0
Matias <mmatidelga2@gmail.com>
2025-04-01 14:54:15 -0300

    feat: enhance generic dialog with focus trapping and update new field dialog input

--- src/app/common/components/generic-dialog/generic-dialog.component.ts
+++ src/app/common/components/generic-dialog/generic-dialog.component.ts
@@ -1,10 +1,11 @@
+import { CdkTrapFocus } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, contentChild, input, TemplateRef } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatDialogModule, CommonModule],
+  imports: [MatDialogModule, CommonModule, CdkTrapFocus],
   selector: 'f5pi-generic-dialog',
   styleUrl: './generic-dialog.component.css',
   templateUrl: './generic-dialog.component.html',
