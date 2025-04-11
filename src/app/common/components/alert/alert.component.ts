def7f898befc6e3cc1dd94172caa6dd5e6d2115f
Matias <mmatidelga2@gmail.com>
2025-04-11 15:35:22 -0300

    feat: add alert component with styles and template

---
+++ src/app/common/components/alert/alert.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatIconModule],
+  selector: 'f5pi-alert',
+  styleUrl: './alert.component.css',
+  templateUrl: './alert.component.html',
+})
+export class AlertComponent {
+  message = input<string>('');
+}
