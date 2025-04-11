98100a7bc0963ea37aad8dcde2a97e60b7e44d5c
Matias <mmatidelga2@gmail.com>
2025-04-11 15:36:11 -0300

    feat: add theme toggle button to sidenav and update styles

--- src/app/common/components/sidenav/sidenav.component.ts
+++ src/app/common/components/sidenav/sidenav.component.ts
@@ -1,16 +1,23 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { SidenavItemComponent } from '@common/components/sidenav-item/sidenav-item.component';
+import { ClientStorageService } from '@common/services/client-storage.service.abstract';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatSidenavModule, SidenavItemComponent],
+  imports: [MatSidenavModule, SidenavItemComponent, MatButtonModule, MatIconModule],
   selector: 'f5pi-sidenav',
   styleUrl: './sidenav.component.scss',
   templateUrl: './sidenav.component.html',
 })
 export class SidenavComponent {
+  document = inject(DOCUMENT);
+  clientStorage = inject(ClientStorageService);
+  currentTheme = signal(this.clientStorage.get('theme') || 'light');
   sidebarItems = signal([
     {
       path: '/',
@@ -28,4 +35,11 @@ export class SidenavComponent {
       label: 'Fields',
     },
   ]);
+
+  toggleTheme() {
+    const newTheme = this.currentTheme() === 'light' ? 'dark' : 'light';
+    this.document.body.setAttribute('data-theme', newTheme);
+    this.clientStorage.set('theme', newTheme);
+    this.currentTheme.set(newTheme);
+  }
 }
