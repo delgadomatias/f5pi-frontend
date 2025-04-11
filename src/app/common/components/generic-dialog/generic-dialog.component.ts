32857073d049e5ab56de59e87b2b12b57187aa17
Matias <mmatidelga2@gmail.com>
2025-04-11 15:35:58 -0300

    feat: enhance generic dialog with footer actions and update styles

--- src/app/common/components/generic-dialog/generic-dialog.component.ts
+++ src/app/common/components/generic-dialog/generic-dialog.component.ts
@@ -1,11 +1,13 @@
 import { CdkTrapFocus } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, contentChild, input, TemplateRef } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatDialogModule, CommonModule, CdkTrapFocus],
+  imports: [MatDialogModule, CommonModule, CdkTrapFocus, MatButtonModule, MatProgressSpinnerModule],
   selector: 'f5pi-generic-dialog',
   styleUrl: './generic-dialog.component.css',
   templateUrl: './generic-dialog.component.html',
@@ -13,5 +15,5 @@ import { MatDialogModule } from '@angular/material/dialog';
 export class GenericDialogComponent {
   dialogTitle = input.required<string>();
   content = contentChild.required('content', { read: TemplateRef });
-  footer = contentChild.required('footer', { read: TemplateRef });
+  isPending = input<boolean>(false);
 }
