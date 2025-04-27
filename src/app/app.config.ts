3a93310d9f5757cf88f73afc34483c18664d5082
Matias <mmatidelga2@gmail.com>
2025-04-27 01:40:16 -0300

    feat: ensure queries use infinite stale time for improved data freshness

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -20,6 +20,7 @@ export const appConfig: ApplicationConfig = {
         defaultOptions: {
           queries: {
             placeholderData: keepPreviousData,
+            staleTime: Infinity
           },
         },
       })
