e87da08cb82cc78af8c5ef912c359fe3f1a53062
Matias <mmatidelga2@gmail.com>
2025-05-29 18:27:01 -0300

    fix(app-config): add HTTP_INTERCEPTORS provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { ErrorStateMatcher, provideNativeDateAdapter, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
@@ -7,6 +7,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideRouter } from '@angular/router';
 import { keepPreviousData, provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental';
 
+import { AuthInterceptor } from '@common/interceptors/auth.interceptor';
 import { ClientStorageService } from '@common/services/client-storage.service.abstract';
 import { LocalStorageService } from '@common/services/local-storage.service';
 import { routes } from './app.routes';
@@ -35,5 +36,6 @@ export const appConfig: ApplicationConfig = {
     },
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline', subscriptSizing: 'dynamic' } },
     { provide: ClientStorageService, useClass: LocalStorageService },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
 };
