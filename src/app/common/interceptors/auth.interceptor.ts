221ec699175e531b3608e092c40ed8f8bd8a1f3c
Ezequiel Sosa <ezequielsosa423@gmail.com>
2025-05-29 15:50:03 -0300

    ref: using access token from auth service

--- src/app/common/interceptors/auth.interceptor.ts
+++ src/app/common/interceptors/auth.interceptor.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '@auth/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  private readonly EXCLUDED_BEARER_ROUTES = ['/auth'];
+  private readonly _EXCLUDED_BEARER_ROUTES = ['/auth'];
+  private readonly _authService = inject(AuthService);
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const accessToken = this.getAccessToken();
-
-    if (!accessToken) return next.handle(req);
     if (this.shouldExcludeBearerRoute(req.url)) return next.handle(req);
+
+    const accessToken = this._authService.accessToken();
+    if (!accessToken) return next.handle(req);
 
     const authReq = req.clone({
       setHeaders: { Authorization: `Bearer ${accessToken}` },
@@ -20,10 +22,6 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   private shouldExcludeBearerRoute(url: string): boolean {
-    return this.EXCLUDED_BEARER_ROUTES.some((route) => url.includes(route));
-  }
-
-  private getAccessToken(): string | null {
-    return localStorage.getItem('accessToken');
+    return this._EXCLUDED_BEARER_ROUTES.some((route) => url.includes(route));
   }
 }
