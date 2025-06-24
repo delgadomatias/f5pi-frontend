09c86d1a2a68bddf4e67f2345413fabb67d90d60
Matias <mmatidelga2@gmail.com>
2025-06-24 16:18:40 -0300

    refactor(common): update paginated interfaces

--- src/app/common/interfaces/paginated-request.interface.ts
+++ src/app/common/interfaces/paginated-request.interface.ts
@@ -1,3 +1,4 @@
 export interface PaginatedRequest {
-  pageNumber: number;
+  pageNumber?: number;
+  pageSize?: number;
 }
--- src/app/common/interfaces/paginated-response.interface.ts
+++ src/app/common/interfaces/paginated-response.interface.ts
@@ -2,6 +2,8 @@ export interface PaginatedResponse<T> {
   content: T[];
   totalElements: number;
   totalPages: number;
+  first: boolean;
+  last: boolean;
   number: number;
   pageable: {
     pageNumber: number;
