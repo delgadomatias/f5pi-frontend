699c3d8156d064d25a825a358f8f172c01fa978e
Matias <mmatidelga2@gmail.com>
2025-06-25 20:23:15 -0300

    refactor(server): remove netlify config

--- src/server.ts
+++ src/server.ts
@@ -1,56 +1,35 @@
-// import { AngularNodeAppEngine, isMainModule, writeResponseToNodeResponse } from '@angular/ssr/node';
-// import compression from 'compression';
-// import express from 'express';
-// import { join } from 'node:path';
+import { AngularNodeAppEngine, isMainModule, writeResponseToNodeResponse } from '@angular/ssr/node';
+import compression from 'compression';
+import express from 'express';
+import { join } from 'node:path';
 
-// const browserDistFolder = join(import.meta.dirname, '../browser');
-// const app = express();
-// const angularApp = new AngularNodeAppEngine();
+const browserDistFolder = join(import.meta.dirname, '../browser');
+const app = express();
+const angularApp = new AngularNodeAppEngine();
 
-// app.use(compression());
-// app.use(
-//   express.static(browserDistFolder, {
-//     maxAge: '1y',
-//     index: false,
-//     redirect: false,
-//   })
-// );
+app.use(compression());
+app.use(
+  express.static(browserDistFolder, {
+    maxAge: '1y',
+    index: false,
+    redirect: false,
+  })
+);
 
-// app.use((req, res, next) => {
-//   angularApp
-//     .handle(req)
-//     .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
-//     .catch(next);
-// });
+app.use((req, res, next) => {
+  angularApp
+    .handle(req)
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
+    .catch(next);
+});
 
-// if (isMainModule(import.meta.url)) {
-//   const port = process.env['PORT'] || 4200;
-//   app.listen(port, (error) => {
-//     if (error) {
-//       throw error;
-//     }
+if (isMainModule(import.meta.url)) {
+  const port = process.env['PORT'] || 4200;
+  app.listen(port, (error) => {
+    if (error) {
+      throw error;
+    }
 
-//     console.log(`Node Express server listening on http://localhost:${port}`);
-//   });
-// }
-
-import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
-import { getContext } from '@netlify/angular-runtime/context.mjs';
-
-const angularAppEngine = new AngularAppEngine();
-
-export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-  const context = getContext();
-
-  // Example API endpoints can be defined here.
-  // Uncomment and define endpoints as necessary.
-  // const pathname = new URL(request.url).pathname;
-  // if (pathname === '/api/hello') {
-  //   return Response.json({ message: 'Hello from the API' });
-  // }
-
-  const result = await angularAppEngine.handle(request, context);
-  return result || new Response('Not found', { status: 404 });
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
 }
-
-export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
