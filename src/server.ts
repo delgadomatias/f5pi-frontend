ab807907478fc652e395ce872f383fd6492799f3
Matias <mmatidelga2@gmail.com>
2025-06-25 18:52:40 -0300

    refactor(server): add netlify support

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,6 @@ import compression from 'compression';
 import express from 'express';
 import { join } from 'node:path';
 
-import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
-import { getContext } from '@netlify/angular-runtime/context.mjs';
-
 const browserDistFolder = join(import.meta.dirname, '../browser');
 const app = express();
 const angularApp = new AngularNodeAppEngine();
@@ -36,13 +33,3 @@ if (isMainModule(import.meta.url)) {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
-
-const angularAppEngine = new AngularAppEngine();
-
-export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-  const context = getContext();
-  const result = await angularAppEngine.handle(request, context);
-  return result || new Response('Not found', { status: 404 });
-}
-
-export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
